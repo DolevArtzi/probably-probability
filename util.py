84cf512202215e4397950f043dfccfe1447ab1f8
Dolev Artzi <dartzi@andrew.cmu.edu>
2023-04-07 01:04:30 -0400

    add hoeffdings for bin and general, aggregate=True in simAll

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import math
+
 from RandomVariable import RandomVariable
 from Binomial import Binomial
 from Uniform import Uniform
@@ -34,6 +36,28 @@ class Util:
         if a > 0:
             return X.variance() / (a * a)
 
+    def hoeffdingsBin(self,X,λ,upperTail=True):
+        μ = X.expectedValue()
+        if upperTail:
+            return math.exp(-(λ**2)/(2*μ + λ))
+        return math.exp(-(λ**2)/(3*μ))
+
+    """
+    Sn = Σ_i X_i, X_i independent RVs in [0,1]
+    E[Sn] = μ
+    then for all λ >= 0: 
+        
+        P[Sn >= μ + λ] <= e^(-λ^2/(2μ + λ))
+        
+        P[Sn <= μ - λ] <= e^(-λ^2/3μ)
+    
+    """
+    def hoeffdings(self,Sn,λ,upperTail=True):
+        μ = sum([X.expectedValue() for X in Sn])
+        if upperTail:
+            return math.exp(-(λ**2)/(2*μ + λ))
+        return math.exp(-(λ**2)/(3*μ))
+
     def flipFairCoin(self,n):
         return self.flipCoin(n,.5)
 
@@ -47,19 +71,19 @@ class Util:
     def simAll(self,k):
         res = []
         avgs = {}
-        print('=' * 50)
+        print('=' * 75)
         print(f'---  Statistics for k = {k} iterations ---')
         for rv_name in self.rvs:
             rv, conditions = self.rvs[rv_name]
             X = rv(*conditions)
-            r = X.simulate(k,aggregate=False)
+            r = X.simulate(k)
             res.append(r)
             if r:
                 avgs[rv_name] = sum(r) / k
             else:
                 avgs.append(0)
-            print(f'{X}: Average = {avgs[rv_name]}')
-        print('=' * 50)
+            # print(f'{X}: Average = {avgs[rv_name]:.4f}')
+        print('=' * 75)
 
     """
     Returns a random instance of an RV given the name of the distribution and its necessary parameters
@@ -75,12 +99,4 @@ class Util:
 u = Util()
 
 if __name__ == '__main__':
-    # x = HyperGeometric(1,10,10)
-    # print(x._slowInverseTransform())
-    # print(x.genVar())
-    # u.simAll(100)
-
-    X = Binomial(20,.5)
-    U = Uniform(0,1).genVar()
-    print(X.genVar(U))
-    print(X.inverseTransform(X.p / (1 - X.p), X.qn,lambda j: (X.n - j) / (1 + j),U))
+    u.simAll(10000)
