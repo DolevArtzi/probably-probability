8a816f182ae9a718052c65ea9666bde76894de8a
Dolev Artzi <dartzi@andrew.cmu.edu>
2023-04-17 10:42:55 -0400

    guessing v0 for util.py, guess() works, guess2() doesn't currently

--- util.py
+++ util.py
@@ -1,15 +1,8 @@
 import math
 
 from RandomVariable import RandomVariable
-from Binomial import Binomial
-from Uniform import Uniform
-from Bernoulli import Bernoulli
-from Geometric import Geometric
-from HyperGeometric import HyperGeometric
-from Poisson import Poisson
-from Exponential import Exponential
-from Normal import Normal
-
+from allRVs import *
+from combine import Combine
 class Util:
     def __init__(self):
         self.rvs = {'binomial':(Binomial,(20,.5)),
@@ -19,7 +12,8 @@ class Util:
                     'hyper geometric':(HyperGeometric, (20,20,20)),
                     'poisson':(Poisson,(12,)),
                     'exponential':(Exponential, (1/10,)),
-                    'normal':(Normal, (0,1))
+                    'normal':(Normal, (0,1)),
+                    'erlang':(Erlang, (3,1/3.5))
                     }
     """
     Markov's Inequality
@@ -107,12 +101,14 @@ class Util:
         avg = sum(data) / k
         avg_var = sum([(x - avg) ** 2 for x in data]) / k
         return avg,avg_var
-    def guess(self,data=None,k=None,target=None,verbose=False):
+    def guess(self,data=None,k=None,target=None,verbose=False,map=None):
+        if not map:
+            map = self.rvs
         if data:
             avg,var = self._avgVar(data)
             m = {}
-            for rv_name in self.rvs:
-                rv, conditions = self.rvs[rv_name]
+            for rv_name in map:
+                rv, conditions = map[rv_name]
                 X = rv(*conditions)
                 m[str(X)] = X.expectedValue(),X.variance()
             best = float('inf')
@@ -130,11 +126,100 @@ class Util:
             return name
         if k != None and target:
             data = [target.genVar() for _ in range(k)]
-            return self.guess(data)
+            return self.guess(data,verbose=verbose,map=map)
         return -1
+
+    def improve(self,data,*conditions,X,avg,var,rv):
+        alpha = 1.05
+        d = 10
+        m = self.calcDiff(X,*conditions,avg,var)
+        best = conditions
+        for i, c in enumerate(conditions):
+            curr = c
+            diff = self.calcDiff(X,avg,var)
+            new_diff = 0
+            f = 0
+            for dir in [-1,1]:
+                while (not f or new_diff < diff) and \
+                    X.valid(curr,*tuple(list(conditions)[:i].extend(list(conditions[i+1:])))):
+                    c1 = tuple(list(conditions)[:i].extend(list(conditions[i+1:])))
+                    print(str(X),c1)
+                    X = rv(*c1)
+                    curr *= dir * alpha
+                    new_diff = self.calcDiff(X,*c1,avg)
+                    if new_diff < m:
+                        m = new_diff
+                        best = c1
+        return m,best
+
+
+
+    def calcDiff(self,X,*cond,avg,var):
+        return abs(X._expectedValue(*cond) - avg) #+ abs(X.variance() - var)
+
+    def guess2(self,data=None,k=None,target=None,verbose=False,map=None):
+        if not map:
+            map = self.rvs
+        if data:
+            avg,var = self._avgVar(data)
+            m = {}
+            for rv_name in map:
+                rv, conditions = map[rv_name]
+                X = rv(*conditions)
+                # m[str(X)] = X.expectedValue(),X.variance()
+                print(self.improve(data,*conditions,map,X,avg,var))
+            best = float('inf')
+            name = None
+            r = []
+            for x in m:
+                diff = abs(m[x][0] - avg) + abs(m[x][1] - var)
+                r.append((x,diff))
+                if diff < best:
+                    best = diff
+                    name = x
+            if verbose:
+                print(r)
+            print(f'Best fit: {name}')
+            return name
+        if k != None and target:
+            data = [target.genVar() for _ in range(k)]
+            return self.guess2(data,verbose=verbose,map=map)
+        return -1
+
+    def iterate(self,f,vars=None,print=False):
+        if not vars:
+            vars = self.rvs
+        if print:
+            r = []
+        for rv_name in vars:
+            rv, conditions = vars[rv_name]
+            X = rv(*conditions)
+            if print:
+                r.append(f(X))
+            else:
+                f(X)
+        if print:
+            print(f'Outcomes: {r}')
+
+    def compareAll2ndMoments(self,rv_names):
+        m = {}
+        for s in rv_names:
+            m[s] = self.rvs[s]
+        self.iterate(RandomVariable.confirm2ndMoment,vars=m)
+
+
+
 u = Util()
 
 
 if __name__ == '__main__':
-    for rv_name in u.rvs:
-        print(rv_name,u.guess(None,1000,u.rvs[rv_name][0](*(u.rvs[rv_name][1]))))
+    # u.guess(None,1000,Erlang(7,2),verbose=True)#map={x:u.rvs[x] for x in u.rvs if x in ['exponential','geometric']})
+#     for rv_name in u.rvs:
+#         print(rv_name,u.guess(None,1000,u.rvs[rv_name][0](*(u.rvs[rv_name][1]))))
+
+    # u.simAll(60000)
+    u.compareAll2ndMoments(['erlang','binomial','bernoulli','exponential'])
+    # u.guess2()
+    # u.guess2(None,1000,Poisson(12),verbose=True)#map={x:u.rvs[x] for x in u.rvs if x in ['exponential','geometric']})
+    # X = Binomial(20,.5)
+    # X.foo()
