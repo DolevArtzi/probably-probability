596cb2441341971920f8ea276f442d5153f5423b
Dolev Artzi <dartzi@andrew.cmu.edu>
2023-04-17 19:32:27 -0400

    add generic plot function and cdf/pdf plotting

--- plot.py
+++ plot.py
@@ -1,51 +1,71 @@
 from allRVs import *
 from util import Util
-from combine import Combine
-
-util = Util()
-combine = Combine()
-
 from matplotlib import pyplot as plt
 
-
+util = Util()
 class Plot:
-    def plotPDF(self,X:RandomVariable,δ=0.05,conditions=None,m=None,other_name=None,other_δ=None):
-        self._plotPDF(X,δ,conditions,m,other_name,other_δ)
-        plt.show()
-        plt.close()
+    def plotPDF(self,X:RandomVariable,mx=None,δ=0.05):
+        self.plot({X.name:([X.params],mx if mx != None else 2 * X.expectedValue(),δ)},'pdf')
 
-    def _plotPDF(self,X:RandomVariable,δ=0.05,conditions=None,m=None,other_name=None,other_δ=None):
-        if conditions:
-            if not other_name:
-                other_name = X.name
-            rv = util.rvs[other_name][0]
+    def plotCDF(self,X:RandomVariable,mx=None,δ=0.05):
+        self.plot({X.name:([X.params],mx if mx != None else 2 * X.expectedValue(),δ)},'cdf')
+
+
+    """
+    m: a map of the form
+        k: RV class name (eg 'binomial')
+        v: (conditions, mx, δ)
+        
+        conditions: a list of parameters to be instantiated with k eg. [(20,.5), (20,.7)]
+        mx = max value to plot for distribution k
+        δ = step value for calculation
+
+    f: the name of a function of type RandomVariable -> Real Numbers that belongs to all RVs
+    
+    together: 
+        true: all plotted on same graph
+        false: different graphs for each instance
+    """
+    def plot(self,m,f,together=True):
+        legend = []
+        m = {util.rvs[k][0]:m[k] for k in m}
+        for rv in m:
+            print(m)
+            conditions, mx, δ = m[rv]
             for c in conditions:
-                if other_δ:
-                    d = other_δ
+                print(rv)
+                print(rv(*c))
+                X = rv(*c)
+                self._plot(X,mx,δ,f)
+                if not together:
+                    plt.legend([f'{X}'])
+                    plt.show()
+                    plt.close()
                 else:
-                    d = δ
-                    self._plotPDF(rv(*c),δ=d,m=m)
+                    legend.append(f'{X}')
+        if together:
+            plt.legend(legend)
+            plt.show()
+            plt.close()
+
+    def _plot(self,X,mx,δ,f):
         x, y = [],[]
-        m1,m2 = X.getMin(),X.getMax()
-        if not m:
-            m = m2
+        m1 = X.getMin()
         MAX = 200
         i = 0
         if not X.strictLower:
             m1 += δ
-        while m1 <= m and i <= MAX:
+        if X.isDiscrete():
+            δ = max(δ,1)
+
+        while m1 <= mx and i <= MAX:
             x.append(m1)
-            y.append(X.pdf(m1))
-            print(x[-1],y[-1])
+            F = getattr(X,f)
+            y.append(F(m1))
             m1+=δ
             i+=1
-        plt.plot(x, y, label=f'{"PMF" if X.isDiscrete() else "PDF"} of {X}')
+        plt.plot(x, y)
 
 if __name__ == '__main__':
     P = Plot()
-    # P.plotPDF(Binomial(1000,.01),1,conditions=[(10,)],m=30,other_name='poisson')
-    P.plotPDF(Binomial(3000,.00333333),1,conditions=[(10,)],m=30,other_name='poisson')
-
-    # P.plotPDF(Poisson(10),.05,m=30)
-    # P.plotPDF(Exponential(10),δ=0.01,m=1)
-    # P.plotPDF(Binomial(20,.7),1)
+    P.plotPDF(Geometric(.01))
