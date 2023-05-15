9980deef81f4a546bef9c7a769435f1cf07c62cd
Dolev Artzi <dartzi@andrew.cmu.edu>
2023-05-14 20:17:10 -0700

    include subdirectories, exclude venv

--- lines.sh
+++ lines.sh
@@ -1,7 +1,6 @@
 c=0
-for f in *.py; do
-    l=$(< $f wc -l | xargs);
+for file in $(find ./ -name '*.py' -type f -not -path './/venv/*'); do
+    l=$(< $file wc -l | xargs);
     c=$(($c+$l));
-
 done
 echo "Total Lines = ${c}"
