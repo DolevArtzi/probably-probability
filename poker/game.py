59304f495db49a3234a0af52c9b310c52ba0ca62
Dolev Artzi <dartzi@andrew.cmu.edu>
2024-01-27 22:25:19 -0500

    rename kwargs to game.start and clean

--- poker/game.py
+++ poker/game.py
@@ -16,63 +16,30 @@ class Game:
         if not statMode and start:
             self.start()
 
-    def start(self,statMode=False,_probMode=True,_print=True,_display=False,_sort=True,_inject=[]):
+    def start(self,statMode=False,probMode=True,printMode=True,displayMode=False,sortMode=True,inject=[]):
         self.t.reset()
-        if _inject != []:
+        if inject != []:
             injected_hands = []
-            if len(_inject) > 2: # not (cot,hands), just cot
-                injected_cot = _inject
+            if len(inject) > 2: # not (cot,hands), just cot
+                injected_cot = inject
             else:
-                injected_cot, injected_hands = _inject
+                injected_cot, injected_hands = inject
             self.t.inject(injected_cot,injected_hands)
         self.t.deal()
-        # x = self.e.c._getOppCombos(self.t.players[0],self.t)
-        # y = self.e.c._getPlayerCombos(self.t.players[0],self.t)
-        # l1 = len(x)
-        # l2 = len(y)
-        # print(self.e.classProbabilities(y))
-        # print(l1,l2,l1*l2)
 
         self.t.progress()
-        if _probMode:
-            x = self.e.c._getOppCombos(self.t.players[0],self.t)
-            y = self.e.c._getPlayerCombos(self.t.players[0],self.t)
-            l1 = len(x)
-            l2 = len(y)
-            # print(self.e.classProbabilities2(y))
-            best_score, p, probs = self.e.probOfImprovement(y,self.t.players[0],self.t)
-            print(f'Best Guaranteed: {best_score}, Prob. of Improving: {p}')
-            m = {k:f'{probs[k]:.6f}' for k in probs}
-            print(f'Prob. of Improving to Score Class: {m}')
-            # print(l1,l2,l1*l2)
+        if probMode:
+            self._getProbs()
             print()
         self.t.progress()
-        if _probMode:
-            x = self.e.c._getOppCombos(self.t.players[0],self.t)
-            y = self.e.c._getPlayerCombos(self.t.players[0],self.t)
-            l1 = len(x)
-            l2 = len(y)
-            # print(self.e.classProbabilities2(y))
-            best_score, p, probs = self.e.probOfImprovement(y,self.t.players[0],self.t)
-            print(f'Best Guaranteed: {best_score}, Prob. of Improving: {p}')
-            m = {k:f'{probs[k]:.6f}' for k in probs}
-            print(f'Prob. of Improving to Score Class: {m}')
-            # print(l1,l2,l1*l2)
-
+        if probMode:
+            self._getProbs()
         self.t.progress()
-        if _probMode:
-            x = self.e.c._getOppCombos(self.t.players[0],self.t)
-            y = self.e.c._getPlayerCombos(self.t.players[0],self.t)
-            l1 = len(x)
-            l2 = len(y)
-            # print(self.e.classProbabilities2(y))
-            # best_score, probs = self.e.probOfImprovement(y,self.t.players[0],self.t)
-            # print(f'Best Score Guaranteed: {best_score}')        
-            # print(l1,l2,l1*l2)
+        
         if not statMode:
-            if _print:
+            if printMode:
                 self.t.printTable()
-            if _display:
+            if displayMode:
                 open_cards = self.t.printCards(self.t.table,ret=True)
                 self.disp.split_display(open_cards,w=90)
             best_hands = []
@@ -81,16 +48,16 @@ class Game:
             for p in self.t.players:
                 x = self.e.evaluate(p,self.t)
                 scores.append(x[0])
-                if _display:
+                if displayMode:
                     cards.append(self.t.printHand(p,retCards=True))
                     print(f'Player {p}: {self.e.strength[x[0]]}')
                     l = x[-1]
-                    if _sort:
+                    if sortMode:
                         best_hand = self.e.s.sortHand(l)
                     else:
                         best_hand = l
                     best_hands.append(best_hand)
-            if _print:
+            if printMode:
                 winner_idxs = self.e.determineWinner(best_hands,scores)
                 if len(winner_idxs) == 1:
                     print(f'Winner: Player {winner_idxs[0]}')
@@ -98,12 +65,21 @@ class Game:
                     print(f'Winners: Players {winner_idxs}')
             for i in range(len(best_hands)):
                 best_hands[i] = self.t.printCards(best_hands[i],ret=True)
-            if _display:
+            if displayMode:
                 cards = [''.join((list(c)[1:])[:-1]).split(',') for c in cards]
                 self.disp.display_all_hands(cards)
                 self.disp.display_all_hands(best_hands)
         else:
             return self.e.evaluate(self.t.players[0],self.t)
+
+    def _getProbs(self):
+        x = self.e.c._getOppCombos(self.t.players[0],self.t)
+        y = self.e.c._getPlayerCombos(self.t.players[0],self.t)
+        best_score, p, probs = self.e.probOfImprovement(y,self.t.players[0],self.t)
+        print(f'Best Guaranteed: {best_score}, Prob. of Improving: {p}')
+        m = {k:f'{probs[k]:.6f}' for k in probs}
+        print(f'Prob. of Improving to Score Class: {m}')
+
 
 if __name__ == '__main__':
     g = Game()
